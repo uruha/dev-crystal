274c6e1314d98d5862bc39db4ffcd132e22ba96e
uruha <kochmusica@gmail.com>
2018-12-16 16:31:26 +0900

    add login

--- src/kemal.cr
+++ src/kemal.cr
@@ -1,4 +1,38 @@
 require "kemal"
+require "multi_auth"
+require "base64"
+
+MultiAuth.config("github", "4da9cc8ec5c3db8a4c0b", "b6ccc7b0613741d8031e3305b1d8bac8082ae194")
+
+def self.multi_auth(env)
+  provider = env.params.url["provider"]
+  redirect_uri = "#{Kemal.config.scheme}://#{env.request.host_with_port.as(String)}/auth/#{provider}/callback"
+  MultiAuth.make(provider, redirect_uri)
+end
+
+get "/auth/:provider" do |env|
+  env.redirect(multi_auth(env).authorize_uri)
+end
+
+get "/auth/:provider/callback" do |env|
+  user = multi_auth(env).user(env.params.query)
+  # p user
+  # p user.name
+  # p user.email
+  # p user.image
+  # p user.nickname
+
+  enc_uname = Base64.encode(user.name)
+  auth_cookie = HTTP::Cookie.new(
+    name: "github_auth",
+    value: enc_uname,
+    path: "/"
+  )
+  set_cookie = auth_cookie.to_set_cookie_header()
+  p set_cookie
+  env.response.headers["Set-Cookie"] = set_cookie
+  env.redirect "/chatroom"
+end
 
 ["/", "/articles"].each do |path|
   get path do |env|
@@ -21,7 +55,7 @@ end
 
 SOCKETS = [] of HTTP::WebSocket
 
-get "/chatroom" do |socket|
+get "/chatroom" do |env|
   title = "Chat"
   render "src/views/chatroom.ecr"
 end
@@ -46,4 +80,5 @@ static_headers do |response, filepath, filestat|
   response.headers.add("Content-Size", filestat.size.to_s)
 end
 
+Kemal.config.port = 2240
 Kemal.run
